73636b2a59ddc034681915ddd6da4b15cfc8235b
mk-kimjh <mk.kimjh@gmail.com>
2016-10-23 20:33:11 +0900

    퍼블리싱 시작, 레이아웃 작성 중...

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>스퀘어원</title>
+    <link href="/assets/fontello/css/fontello.css" rel="stylesheet">
+    <link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="/assets/css/default.css" rel="stylesheet">
+</head>
+<body>
+    <div id="top-bn">
+    </div>
+    <header id="header">
+        <div id="top-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-2 col-lg-3">
+                        <button class="btn btn-menu hidden-lg" type="button">
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="sr-only">메뉴버튼</span>
+                        </button>
+                        <ul class="member visible-lg">
+                            <li><a href="#">HOME</a></li>
+                            <li><a href="#">LOGIN</a></li>
+                            <li><a href="#">JOIN</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-8 col-lg-4 col-lg-offset-1">
+                        <h1 class="logo"><a href="/"><img src="/assets/images/logo.png" alt="스퀘어원"></a></h1>
+                    </div>
+                    <div class="col-xs-2 col-lg-2">
+                        <ul class="sns visible-lg">
+                            <li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+                            <li><a class="blog" href="#" aria-label="블로그"><span aria-hidden="true">b</span></a></li>
+                        </ul>
+                        <button class="btn btn-search hidden-lg" type="button" aria-label="검색">
+                            <i class="icon-search"></i>
+                        </button>
+                        <div class="search-area visible-lg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav id="gnb">
+            <div class="container">
+                <div class="row">
+                    GNB
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main style="height: 200px;">
+
+    </main>
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10">
+                    <ul class="policy">
+                        <li><a href="#">회사소개</a></li>
+                        <li><a href="#">이용약관</a></li>
+                        <li><a href="#">개인정보취급방침</a></li>
+                        <li><a href="#">이메일무단수집거부</a></li>
+                        <li><a href="#">CONTACT US</a></li>
+                    </ul>
+                    <ul class="company">
+                        <li>(주)서부T&amp;D 인천광역시 연수구 청능대로 210</li>
+                        <li>전화번호 : 032-456-4000</li>
+                        <li>팩스번호 : 032-456-4667</li>
+                    </ul>
+                    <ul class="business">
+                        <li>대표이사 : 송민호</li>
+                        <li>사업자등록번호 : 220-81-666565</li>
+                    </ul>
+                    <p>(C) 2016 SEOBU T&amp;D CO.,LTD  ALL RIGHT RESERVED. <a href="http://www.movementk.com" target="_blank">SITE BY MOVEMENTK GROUP.</a></p>
+                </div>
+                <div class="col-lg-2">
+                    <ul class="sns">
+                        <li><a href="#">페이스북</a></li>
+                        <li><a href="#">블로그</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
