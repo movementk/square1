6aa2d42064c263a264394e188c2816b53008f202
kyh5392 <kyh90495392@gmail.com>
2016-11-14 18:52:23 +0900

    서브페이지 퍼블리싱 #5 소개부분

--- sub.php
+++ sub.php
@@ -28,6 +28,46 @@
             <!-- 가이드 작성 영역 -->
             <div class="container">
                 <div style="margin: 30px 0;">
+                    <p class="reference">
+                        금액권 사용 불가 매장 : 홈플러스, CGV, CU편의점, 약국, 병원,  LG하우스, 게임센터
+                    </p>
+                    <ul class="dot-list">
+                        <li>카드 발급</li>
+                        <li>포이트 적립</li>
+                    </ul>
+
+                    <nav aria-label="Page navigation" class="paging">
+                        <ul class="pagination">
+                            <li>
+                                <a href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li><a href="#">5</a></li>
+                            <li>
+                                <a href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                </div>
+                <div style="margin: 30px 0;">
                     <div class="search-form">
                         <form>
                             <div class="form-group">
