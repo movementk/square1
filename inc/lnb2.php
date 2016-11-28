074d1f30e59f54037fa1a17b4b7ae10f4b77dff9
mk-kimjh <mk.kimjh@gmail.com>
2016-11-28 14:40:20 +0900

    매장안내 LNB 링스 추가

--- inc/lnb2.php
+++ inc/lnb2.php
@@ -1,12 +1,12 @@
 <nav id="lnb" class="hidden-xs">
     <div class="container">
         <ul>
-            <li class="active"><a href="#">카테고리별 안내</a></li>
-            <li><a href="#">층별안내</a></li>
-            <li><a href="#">편의시설</a></li>
-            <li><a href="#">영업시간</a></li>
-            <li><a href="#">포인트카드</a></li>
-            <li><a href="#">주차시설</a></li>
+            <li class="active"><a href="/store/category.php">카테고리별 안내</a></li>
+            <li><a href="/store/floor.php">층별안내</a></li>
+            <li><a href="/store/place.php">편의시설</a></li>
+            <li><a href="/store/hours.php">영업시간</a></li>
+            <li><a href="/store/point.php">포인트카드</a></li>
+            <li><a href="/store/parking.php">주차시설</a></li>
         </ul>
     </div>
 </nav>
